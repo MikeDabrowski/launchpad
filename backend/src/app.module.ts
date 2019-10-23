646549299e9503147693a5cc88a6b2a957c5f4a6
MikeDabrowski <mike2060@gmail.com>
2019-10-23 21:43:37 +0200

    works

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PlayModule } from './play/play.module';
+import * as path from "path";
 
 @Module({
   imports: [PlayModule],
 })
 export class AppModule {
+  constructor() {
+    global['rootPath'] = path.resolve(__dirname).replace(/(src\/app|src\\app)/, '');
+  }
 }
